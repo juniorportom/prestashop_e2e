16862d7a9895fa3b82825d10f32544ff77c74356
Armando Acosta Guerrero <darman.89@gmail.com>
2019-02-08 22:27:50 -0500

    Prueba de Ingreso Administrativo

---
+++ cypress/integration/prestashop/login_administrativo_spec.js
@@ -0,0 +1,14 @@
+const email = "tsdc.agile.test@gmail.com", password = "GrupoAgiles";
+const url = 'http://ec2-18-222-141-129.us-east-2.compute.amazonaws.com:8080/admin428l2esux';
+
+
+describe('Acceso Administrativo Prestashop', function() {
+    it('Ingresar a pagina de administración', function() {
+        cy.visit(url)
+        cy.contains('Iniciar sesión').click()
+        cy.get('form#login_form').find('input[name="email"]').click().type(email)
+        cy.get('form#login_form').find('input[name="passwd"]').click().type(password)
+        cy.get('form#login_form').contains('Iniciar sesión').click()
+        cy.contains('Cerrar sesión').click({force:true})
+    })
+})
